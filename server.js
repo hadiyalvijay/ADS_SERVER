0777b10d09ba83e66090134ddf083ddfcf4f5aa4
Hadiyal Vijay <hadiyalvijay7777@gmail.com>
2024-12-16 10:47:02 +0530

    update commit

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require('express');
-const connectDB = require('./db');
+const connectDB = require('./db'); // Assuming you have a database connection file
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-// const employeesRoute = ;
 
 const app = express();
 
+// Connect to database
 connectDB();
 
+// CORS configuration
 app.use(cors({
-    origin: 'https://adsbyuv.surge.sh'
+  origin: 'https://adsbyuv.surge.sh' // Replace with your frontend's origin
 }));
+
+// Parse JSON request bodies
 app.use(bodyParser.json());
 
-// Routes
+// Define your API routes
 app.use('/api/auth', require('./Routes/auth'));
 app.use('/api/employees', require('./Routes/employeeRoutes'));
 
